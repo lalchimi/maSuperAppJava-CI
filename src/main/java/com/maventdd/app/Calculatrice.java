b8f031553e260a819a5a459f708471e11c276fa2
Jerome Labro <lalchimi@hotmail.fr>
2025-05-12 16:46:12 +0200

    fake error

--- src/main/java/com/maventdd/app/Calculatrice.java
+++ src/main/java/com/maventdd/app/Calculatrice.java
@@ -2,6 +2,6 @@ package com.maventdd.app; // Assure-toi que le package est correct
 
 public class Calculatrice {
     public int additionner(int a, int b) {
-        return a + b; // L'implémentation correcte !
+        return a + b + 1; // L'implémentation correcte !
     }
 }
